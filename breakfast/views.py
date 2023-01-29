6e48dc7d251249e4e2bb58dcc97d894cea083d35
alice <minion.banana.poteto2013@gmail.com>
2023-01-29 17:54:05 +0900

    breakfast/views.pyを修正

--- breakfast/views.py
+++ breakfast/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 
@@ -7,5 +8,12 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['message'] = 'こんにちは'
+        context['now'] = datetime.now().strftime('%Y/%m/%d %H:%M')
+        now_hour = datetime.now().hour
+        if 5 <= now_hour < 10:
+            context['message'] = 'おはようございます'
+        elif 10 <= now_hour < 18:
+            context['message'] = 'こんにちは'
+        else:
+            context['message'] = 'こんばんは'
         return context
