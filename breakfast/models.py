f65fd1eddf0dfd975eeeef771b391dca9d19f9b3
alice <minion.banana.poteto2013@gmail.com>
2023-01-31 22:18:55 +0900

    models.pyにclass Breakfastを追加

--- breakfast/models.py
+++ breakfast/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Breakfast(models.Model):
+    date = models.DateField()
+    hours_of_sleep = models.IntegerField()
+    breakfast = models.CharField(max_length=200)
+    sleep_quality = models.IntegerField()
+    feeling = models.IntegerField()
+    sweet = models.CharField(max_length=200)
+    memo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.date
