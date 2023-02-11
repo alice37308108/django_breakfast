d51e7ba289c26d192cd54380b39c1dc6ed31f261
alice <minion.banana.poteto2013@gmail.com>
2023-02-11 23:16:38 +0900

    forms.pyを修正

--- breakfast/forms.py
+++ breakfast/forms.py
@@ -1,23 +1,5 @@
 from django import forms
 
-
-# class BreakfastForm(forms.Form):
-#     date = forms.DateField()
-#     hours_of_sleep = forms.IntegerField()
-#     breakfast = forms.CharField(max_length=200)
-#     sleep_quality = forms.IntegerField()
-#     feeling = forms.IntegerField()
-#     sweet = forms.CharField(max_length=200, required=False)
-#     memo = forms.CharField(max_length=200, required=False)
-
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     for field in self.fields.values():
-    #         field.widget.attrs['class'] = 'form-control'
-
-
-from django import forms
-
 from .models import Breakfast
 
 
@@ -28,8 +10,3 @@ class BreakfastForm(forms.ModelForm):
         # そのフォームの中から表示するフィールドを指定
         fields = ('date', 'hours_of_sleep', 'breakfast', 'sleep_quality', 'feeling', 'sweet', 'memo',)
 
-    # フォームを綺麗にするための記載
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
