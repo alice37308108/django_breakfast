082c8ea5ce54d9986a61e5e719a6a7cf070689ef
alice37308108 <minion.banana.poteto2013@gmail.com>
2023-02-19 15:51:04 +0900

    models.pyを修正

--- breakfast/models.py
+++ breakfast/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Breakfast(models.Model):
-    date = models.DateField()
-    hours_of_sleep = models.IntegerField()
-    breakfast = models.CharField(max_length=200)
-    sleep_quality = models.IntegerField()
-    feeling = models.IntegerField()
-    sweet = models.CharField(max_length=200, null=True, blank=True)
-    memo = models.CharField(max_length=200, null=True, blank=True)
+    date = models.DateField(verbose_name='日付')
+    hours_of_sleep = models.IntegerField(verbose_name='睡眠時間')
+    breakfast = models.CharField(max_length=200,verbose_name='朝ごはん')
+    sleep_quality = models.IntegerField(verbose_name='睡眠の質')
+    feeling = models.IntegerField(verbose_name='今日の気分')
+    sweet = models.CharField(max_length=200, null=True, blank=True,verbose_name='おやつ')
+    memo = models.CharField(max_length=200, null=True, blank=True,verbose_name='メモ')
 
     def __str__(self):
         return self.date.strftime('%Y/%m/%d')
