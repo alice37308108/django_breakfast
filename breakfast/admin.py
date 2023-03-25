84f735dcfe043360f065d800421a9dca2b612f87
alice <minion.banana.poteto2013@gmail.com>
2023-03-25 23:36:56 +0900

    admin.pyを修正

--- breakfast/admin.py
+++ breakfast/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Breakfast
+from .models import Breakfast, Category, Tag
 
-admin.site.register(Breakfast)
+
+@admin.register(Breakfast)
+class BreakfastAdmin(admin.ModelAdmin):
+    list_display = ('date', 'breakfast',)  # 管理サイトでモデルの一覧表示時に表示するフィールドのリストを指定する
+    search_fields = ('date', 'breakfast',)  # 管理サイトでモデルの検索を行う際に検索対象のフィールドを指定する
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
